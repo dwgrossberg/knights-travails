7a803d297653f694b46755dcd9c5db3aef590832
Daniel <dwgrossberg@gmail.com>
2022-09-05 22:14:04 -0400

    Get warnsdorff knights tour function running (finally)

--- src/KnightsTourWarnsdorff.js
+++ src/KnightsTourWarnsdorff.js
@@ -9,49 +9,49 @@ const KnightsTourWarnsdorff = (x, y, N) => {
   if (!origin.checkMove()) return null;
   const n = origin.data.boardSize;
   const visited = [...Array(n)].map(() => Array(n).fill(false));
+  // helper functions
   const everySquareVisited = (board) => {
     return board.every((column) => column.every((square) => square));
   };
   const copyOfVisited = (board) => board.map((column) => column.slice());
+  const getBoardMoves = (array, board) => {
+    const boardMoves = [];
+    array.forEach((move) => {
+      if (board[move[0] - 1][move[1] - 1] === false) boardMoves.push(move);
+    });
+    return boardMoves;
+  };
+  // Tweaking the Knights Tour backtracking algorithm to follow Warnsdorff's heuristics:
+  // "When looking at the next possible moves, prefer the next move with the fewest possible options"
+  const warnsdorff = (KnightMoves, board, boardSize) => {
+    const weightedMoves = [];
+    // find the number of possible paths for each KnightMove and return them in order from fewest to most
+    for (const [x, y] of KnightMoves) {
+      const currentSquare = ChessSquare(x, y);
+      currentSquare.updateBoardSize(boardSize);
+      const moves = currentSquare.possibleMoves();
+      const boardMoves = getBoardMoves(moves, board);
+      const weight = boardMoves.length;
+      weightedMoves.push({ move: [x, y], weight });
+    }
+    return weightedMoves
+      .sort((a, b) => b.weight - a.weight)
+      .map((weighted) => weighted.move);
+  };
+  // main function
   const findPath = (xCoord, yCoord, board, boardSize) => {
     const current = ChessSquare(xCoord, yCoord);
     current.updateBoardSize(boardSize);
     const boardCopy = copyOfVisited(board);
     boardCopy[xCoord - 1][yCoord - 1] = true;
-    let moves = current.possibleMoves();
-    const getBoardMoves = (array) => {
-      const boardMoves = [];
-      array.forEach((move) => {
-        if (boardCopy[move[0] - 1][move[1] - 1] === false)
-          boardMoves.push(move);
-      });
-      return boardMoves;
-    };
-    if (getBoardMoves(moves).length === 0) {
+    const moves = current.possibleMoves();
+    const boardMoves = getBoardMoves(moves, boardCopy);
+    if (boardMoves.length === 0) {
       if (everySquareVisited(boardCopy)) return [[x, y]];
       else return false;
     } else {
-      // Tweaking the Knights Tour backtracking algorithm to follow Warnsdorff's heuristics:
-      // "When looking at the next possible moves, prefer the next move with the fewest possible options"
-      const warnsdorff = (KnightMoves) => {
-        const weightedMoves = [];
-        // find the number of possible paths for each KnightMove and return them in order from fewest to most
-        for (const [x, y] of KnightMoves) {
-          const currentSquare = ChessSquare(x, y);
-          console.log(currentSquare);
-          // const currentMoves = getBoardMoves(currentSquare.possibleMoves());
-          const weight = currentSquare.possibleMoves().length;
-          weightedMoves.push({ move: [x, y], weight });
-        }
-        return weightedMoves
-          .sort((a, b) => b.weight - a.weight)
-          .map((weighted) => weighted.move);
-      };
-      // console.log(moves);
-      moves = warnsdorff(getBoardMoves(moves));
-      console.log(moves);
-
-      for (let [nextX, nextY] of moves) {
+      const warnsdorfffMoves = warnsdorff(boardMoves, boardCopy, boardSize);
+      for (let [nextX, nextY] of warnsdorfffMoves) {
         let path = findPath(nextX, nextY, boardCopy, boardSize);
         if (path) {
           path.push([nextX, nextY]);
